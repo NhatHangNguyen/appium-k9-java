483fc7ece3d26d68b08cbb57c0366d4f233bdcd4
NhatHangNguyen <nhathangnguyen1806@gmail.com>
2022-05-30 18:00:27 +0700

    Lab 02

---
+++ src/lab_02/lab_02_2.java
@@ -0,0 +1,19 @@
+package lab_02;
+
+import java.util.Scanner;
+
+public class lab_02_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input your number: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Your input number is even number");
+        } else {
+            System.out.println("Your input number is odd number");
+        }
+    }
+}
+
+
+
